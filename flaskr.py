aa64768938720cc9687184771ddbbb451755d794
kafey <febri@saklik.com>
2015-06-22 05:52:03 +0700

    setting app awal

--- flaskr.py
+++ flaskr.py
@@ -1,1 +1,24 @@
-from flask import Flask
+# all the imports
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+	abort, render_template, flash
+	
+# configuration
+DATABASE = 'flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+
+# create our little application :)
+app= Flask(__name__)
+#app.config.from_object(__name_)
+app.config.from_envvar('FLASKR_SETTING', silent=True)
+
+def connect_db():
+	return sqlite3.connect(app.config['DATABASE'])
+	
+	
+if __name__ == '__main__':
+	app.run()
