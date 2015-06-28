95cd78bc831a8a5485c87327ba412ddaf128a6f2
kafey <febri@saklik.com>
2015-06-28 13:05:38 +0700

    selesai

--- flaskr.py
+++ flaskr.py
@@ -32,17 +32,51 @@ def before_request():
 
 @app.teardown_request
 def teardown_request(exeption):
-	db = getarr(g, 'db', None)
+	db = getattr(g, 'db', None)
 	if db is not None:
 		db.close()
 		
 @app.route('/')
 def show_entries():
 	cur = g.db.execute('select title, text from entries order by id desc')
+	entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+	return render_template('show_entries.html', entries=entries)
 	
+@app.route('/add', methods=['POST'])
+def add_entry():
+	if not session.get('logged_in'):
+		abort(401)
+	g.db.execute('insert into entries (title, text) values(?,?)',
+					[request.form['title'], request.form['text']])
+	flash('New entry was succesfully posted')
+	return redirect(url_for('show_entries'))
+	
+	
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME']:
+			error = 'Invalid username'
+		elif request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid password'
+		else:
+			session['logged_in'] = True
+			flash('You were logged in')
+			return redirect(url_for('show_entries'))
+	return render_template('login.html', error= error)
+	
+	
+	
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('show_entries'))
 
-	
+
 if __name__ == '__main__':
 	app.run()
+	
 
 
